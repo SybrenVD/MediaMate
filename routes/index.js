eaf747b66e3d36b234b52fb7e522a4d695ae4b2e
SybrenVD <sybren.vanderdonckt@student.vives.be>
2025-04-30 17:22:29 +0200

    start-hero

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Home" });
+  res.render("index", 
+    { 
+      title: "Home",
+      banner: "/images/placeholder.jpg",
+
+
+        cta: "Welcome to MediaMate",
+        shortDescription: "Find the best in entertainment"
+
+    
+      
+      
+    });
 });
 
 
