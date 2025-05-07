d0455bf74802af6d9b4a14bac86e975df0b3a7f7
SybrenVD <sybren.vanderdonckt@student.vives.be>
2025-05-07 14:19:48 +0200

    card-partial + most-viewed

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,21 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", 
-    { 
+router.get("/", function (req, res, next) {
+  res.render("index",
+    {
       title: "Home",
-      banner: "/images/placeholder.jpg",
-
-
+      hero: {
+        banner: "/images/placeholder.jpg",
         cta: "Welcome to MediaMate",
         shortDescription: "Find the best in entertainment"
+      },
+      card: {
+        title: "Title",
+        description: "Description",
+        img: "/images/placeholder.jpg"
+      }
 
-    
-      
-      
     });
 });
 
