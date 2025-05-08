82145af6ff71d9d30055c49fe5a5c4ce28a8e285
BlachAleksandra <r1052586@student.vives.be>
2025-05-08 09:49:59 +0200

    database connected

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//ophalen van de API key uit de .env bestand
+require('dotenv').config();
+
 const createError = require("http-errors");
 const express = require('express');
 const path = require("path");
 const hbs = require("hbs");
 const indexRouter = require("./routes/index");
-
+const axios = require('axios');
+const sql = require('mssql');
 const app = express();
 
 // view engine setup
@@ -34,5 +38,27 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+//Database configuratie
+const dbConfig = {
+  user: process.env.DB_USER,            // Gebruikersnaam van de database (uit .env)
+  password: process.env.DB_PASSWORD,    // Wachtwoord van de database (uit .env)
+  server: process.env.DB_SERVER,        // SQL Server adres (bijv. localhost)
+  database: process.env.DB_NAME,        // Naam van de database
+  options: {
+    encrypt: true,                      // Altijd 'true' voor beveiliging
+    trustServerCertificate: true        // Vertrouwen op het servercertificaat (voor lokale servers)
+  }
+};
+
+// Verbinden met de database
+async function connectToDatabase() {
+  try {
+    await sql.connect(dbConfig);
+    console.log('Verbonden met de database!');
+  } catch (err) {
+    console.error('Fout bij verbinden met de database:', err);
+  }
+}
 
 module.exports = app;
