d2e9cc559fb1371ff6bcbb615dbe6cdc69cd3e03
KeXu <ke.xu@student.vives.be>
2025-05-09 10:43:07 +0200

    groep element fix+ groepcardpartial

--- routes/index.js
+++ routes/index.js
@@ -309,8 +309,14 @@ router.post("/add", function (req, res) {
 
 /*GET groeplist+room */
 router.get("/chatroom", function(req,res){
-  res.render("chatgroep",{
+  res.render("chatgroup",{
     title:"Chatrooms",
+    groups: [
+      {groupId: 0, tag: "", createrId: 0, createDate: Date.now, lidId: 0, groupName: "A", groupImg: "https://huiji-public.huijistatic.com/isaac/uploads/c/c4/Eve%27s_Mascara_Tears.png"},
+      {groupId: 1, tag: "", createrId: 0, createDate: Date.now, lidId: 0, groupName: "B", groupImg: "https://huiji-public.huijistatic.com/isaac/uploads/c/c4/Eve%27s_Mascara_Tears.png"},
+      {groupId: 2, tag: "", createrId: 0, createDate: Date.now, lidId: 0, groupName: "C", groupImg: "https://huiji-public.huijistatic.com/isaac/uploads/c/c4/Eve%27s_Mascara_Tears.png"},
+      {groupId: 3, tag: "", createrId: 0, createDate: Date.now, lidId: 0, groupName: "D", groupImg: "https://huiji-public.huijistatic.com/isaac/uploads/c/c4/Eve%27s_Mascara_Tears.png"}
+    ]
   });
 });
 
